65d79b02a19a05e4f14647e578e69b4a662dbdb1
olinb <olin.bourquin@gmail.com>
2025-06-11 19:20:19 +0200

    fix

--- src/main/java/heigvd/plm/nothello/logic/NotHelloStrategy.java
+++ src/main/java/heigvd/plm/nothello/logic/NotHelloStrategy.java
@@ -1,15 +1,12 @@
 package heigvd.plm.nothello.logic;
 
 import heigvd.plm.nothello.game.Board;
-import heigvd.plm.nothello.game.PieceColor;
-
-import java.util.List;
 
 public interface NotHelloStrategy {
     // Conventions utilisées dans les stratégies :
-    public final int MY_COLOR = -1;
-    public final int OPP_COLOR = 1;
-    public final int EMPTY = 0;
+    int MY_COLOR = -1;
+    int OPP_COLOR = 1;
+    int EMPTY = 0;
 
     /**
      * Retourne le meilleur mouvement possible pour le joueur courant.
