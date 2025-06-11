474a81e92285ac1ffcfa3417ba366b4135b4beff
olinb <olin.bourquin@gmail.com>
2025-06-11 19:19:51 +0200

    fix

--- src/main/java/heigvd/plm/nothello/logic/NotHelloStrategy.java
+++ src/main/java/heigvd/plm/nothello/logic/NotHelloStrategy.java
@@ -18,35 +18,4 @@ public interface NotHelloStrategy {
      * @return Un tableau {x, y} représentant les coordonnées du meilleur coup
      */
     int[] evaluate(Board board);
-
-    /**
-     * Clone profond du plateau.
-     */
-    default Board cloneBoard(Board board) {
-        //System.out.println("\ncall to cloneBoard():");
-        Board clone = new Board();
-
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                PieceColor color = board.getColorAt(i, j);
-                if (!color.isNone()) {
-                    clone.hardSetColorAt(i, j, color);
-                }
-            }
-        }
-
-        // Force le même joueur
-        clone.setPlayerTurn(board.getPlayerTurn());
-
-        /*
-        System.out.println("orginal board:");
-        System.out.println(board.getPlayerTurn());
-        System.out.println(board.toString());
-        System.out.println("copied board:");
-        System.out.println(clone.getPlayerTurn());
-        System.out.println(clone.toString());
-        //*/
-
-        return clone;
-    }
 }
